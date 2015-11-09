30039635b24186189eea194d249739a81c8be54a
mnsekh111 <mnsekh111@gmail.com>
2015-11-09 18:46:12 -0500

    Added login flow and signin button

--- app/src/main/java/com/mns/googleplussignin/MainActivity.java
+++ app/src/main/java/com/mns/googleplussignin/MainActivity.java
@@ -1,18 +1,24 @@
 package com.mns.googleplussignin;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.auth.api.Auth;
 
 public class MainActivity extends AppCompatActivity {
 
     GoogleApiClient mGoogleApiClient;
+    private final int LOGIN_RES = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,35 +27,47 @@ public class MainActivity extends AppCompatActivity {
                 .requestEmail()
                 .build();
 
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this,mOnFailListener)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+
+        SignInButton signInButton = (SignInButton) findViewById(R.id.btn_signin);
+        signInButton.setSize(SignInButton.SIZE_STANDARD);
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+                startActivityForResult(signInIntent, LOGIN_RES);
+            }
+        });
 
     }
 
     GoogleApiClient.OnConnectionFailedListener mOnFailListener = new GoogleApiClient.OnConnectionFailedListener() {
         @Override
         public void onConnectionFailed(ConnectionResult connectionResult) {
-            Toast.makeText(getBaseContext(),"Failed to connect",Toast.LENGTH_LONG).show();
+            Toast.makeText(getBaseContext(), "Failed to connect", Toast.LENGTH_LONG).show();
         }
     };
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == LOGIN_RES) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            if(result != null){
+                if (result.isSuccess()) {
+                    // Signed in successfully, show authenticated UI.
+                    Toast.makeText(getBaseContext(), "Signed in Successfully", Toast.LENGTH_LONG).show();
+                    GoogleSignInAccount acct = result.getSignInAccount();
+                }
+            }
         }
-
-        return super.onOptionsItemSelected(item);
     }
+
+
 }
